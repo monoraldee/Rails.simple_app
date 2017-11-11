1b82b344e879d3c1537e9bae3133b83132e0128b
monoraldee <kazuhiro.egawa@excite.jp>
2017-11-11 21:45:38 +0900

    [modify] tokenを定数から乱数に

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,4 +1,4 @@
-# Be sure to restart your server when you modify this file.
+ure to restart your server when you modify this file.
 
 # Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SimpleApp::Application.config.secret_key_base = '08edf91cfc8731724ef4b9a5b433cfc05e7c0a0c152c80fa3a5c7f4abf99c2cbade0b9a42844752cf97d99790b059037b1b3be74b9379691cb97af304054d36b'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
