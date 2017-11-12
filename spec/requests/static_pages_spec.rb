b8efaf544673328c6a27fc479f4dfcc2a24bcef5
monoraldee <kazuhiro.egawa@excite.jp>
2017-11-12 17:45:16 +0900

    [add] テスト追加

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    it "should have the content 'Simple App'" do
+        visit '/static_pages/home'
+        expect(page).to have_content('Simple App')
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'F**'" do
+        visit '/static_pages/help'
+        expect(page).to have_content('Help Me')
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+        visit '/static_pages/about'
+        expect(page).to have_content('ababa')
+    end
+  end
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  describe "right title test home" do
+    it "should have the right title home" do
+        visit '/static_pages/home'
+        expect(page).to have_title("#{base_title} | Home")
+    end
+  end
+
+  describe "right title test help" do
+    it "should have the right title help" do
+        visit '/static_pages/help'
+        expect(page).to have_title("#{base_title} | Help")
+    end
+  end
+
+  describe "right title about" do
+    it "should have the right title about" do
+        visit '/static_pages/about'
+        expect(page).to have_title("#{base_title} | About")
+    end
+  end
+end
