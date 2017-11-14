ec9d64a97cc01d153bd587b97a69ff511624a49b
monoraldee <kazuhiro.egawa@excite.jp>
2017-11-14 12:38:00 +0900

    [modify] クラスについて実装

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   describe "Home page" do
     it "should have the content 'Simple App'" do
         visit '/static_pages/home'
         expect(page).to have_content('Simple App')
+    end
+
+    describe "right title test home" do
+      it "should have the right title home" do
+          visit '/static_pages/home'
+          expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+      end
+    end
+
+    describe "base title test" do
+      it "should have the right title about" do
+          visit '/static_pages/home'
+          expect(page).not_to have_title("Home")
+      end
     end
   end
 
@@ -19,14 +34,6 @@ describe "StaticPages" do
     it "should have the content 'About Us'" do
         visit '/static_pages/about'
         expect(page).to have_content('ababa')
-    end
-  end
-
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-  describe "right title test home" do
-    it "should have the right title home" do
-        visit '/static_pages/home'
-        expect(page).to have_title("#{base_title} | Home")
     end
   end
 
