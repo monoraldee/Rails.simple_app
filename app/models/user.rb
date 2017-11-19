31e23af4b9cebd5a3828642a4590d841d7c5ac1f
egawakazuhiro <kazuhiro.egawa@excite.jp>
2017-11-19 20:19:48 +0900

    [update] パスワード認証に対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
 
+  has_secure_password
   before_save { self.email = email.downcase }
+
 end
