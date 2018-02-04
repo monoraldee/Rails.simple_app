ad44c334b21a0824181b417f44d07790f006bde3
egawakazuhiro <egawakazuhiro@osiro.it>
2018-02-04 14:16:45 +0900

    [change] 編集機能など追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
-  def show
-    @user = User.find(params[:id])
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def new
     @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def create
@@ -19,7 +25,38 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
+  # Before actions
+
+  def signed_in_user
+    unless signed_in?
+      store_location # 直前url保存
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 end
