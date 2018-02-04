7989f782b20a5fd359fc2f564da49ed0d8267560
egawakazuhiro <egawakazuhiro@osiro.it>
2018-02-04 16:34:47 +0900

    [add] 削除機能追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,12 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:remember_token) }
 
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
   describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
@@ -112,4 +118,14 @@ describe User do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -124,20 +124,4 @@ describe "User pages" do
       end
     end
   end
-
-  it { should respond_to(:authenticate) }
-  it { should respond_to(:admin) }
-
-  it { should be_valid }
-  it { should_not be_admin }
-
-  describe "with admin attribute set to 'true'" do
-    before do
-      @user.save!
-      @user.toggle!(:admin)
-    end
-
-    it { should be_admin }
-  end
-
 end
