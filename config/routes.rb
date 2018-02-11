2b91e46761dad3b193db3383b0f0eda7cee82476
egawakazuhiro <egawakazuhiro@osiro.it>
2018-02-11 16:47:49 +0900

    [add] micropostsモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ SimpleApp::Application.routes.draw do
   match '/signup', to: 'users#new', via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
+  resources :microposts, only: [:create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -4,10 +4,14 @@ FactoryGirl.define do
     sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
+  end
 
-    factory :admin do
-      admin true
-    end
+  factory :admin do
+    admin true
+  end
 
+  factory :micropost do
+    content "Lorem ipsum"
+    user
   end
 end
